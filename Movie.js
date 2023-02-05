2e2489a15572b05d999e29091b2976f367b51ddf
kai <kailashjat604@gmail.com>
2023-02-05 20:05:03 +0530

    Render Movies

---
+++ Movie.js
@@ -0,0 +1,58 @@
+class Movie {
+    constructor(data) {
+        console.log(data);
+        Object.assign(this, data);
+    }
+    /*
+Genre
+: 
+"Drama, Sport"
+Plot
+: 
+"Three friends growing up in India at the turn of the millennium set out to open a training academy to produce the country's next cricket stars."
+Poster
+: 
+"https://m.media-amazon.com/images/M/MV5BMTgwNTAwMjEzMF5BMl5BanBnXkFtZTcwNzMzODY4OA@@._V1_SX300.jpg"
+Ratings
+: 
+(3) [{…}, {…}, {…}]
+Runtime
+: 
+"120 min"
+Title
+: 
+"Kai Po Che"
+imdbRating
+: 
+"7.8"
+*/
+    getMovieHtml() {
+        const {Poster, Title, imdbRating, Runtime, Genre, Plot} = this;
+        return `
+            <div class="movie">
+                <img class="movie-poster" src="${Poster}" alt="${Title}'s Poster" />
+                <div class="movie-details">
+                    <div class="detail-header">
+                        <div class="movie-title">${Title}</div>
+                        <img src="./images/star.png" alt="star-icon">
+                        <div class="movie-rating">${imdbRating}</div>
+                    </div>
+                    <div class="details-main">
+                        <div class="movie-duration">${Runtime}</div>
+                        <div class="movie-genre">${Genre}</div>
+                        <button>
+                            <img src="./images/add-icon.png" alt="add-icon">
+                            Watchlist
+                        </button>
+                    </div>
+                    <div class="description">
+                        ${Plot}
+                    </div>
+                </div>
+            </div>
+            <br class="movie-end">
+        `;
+    }
+}
+
+export default Movie;
--- index.js
+++ index.js
@@ -1,16 +1,49 @@
+import Movie from "./Movie.js";
+
 const searchForm = document.getElementById("search-form");
 const searchTextEl = document.getElementById("search-text");
 const moviesContainer = document.getElementById("movies");
 let moviesData = [];
 
-searchForm.addEventListener("submit", (event) => {
+searchForm.addEventListener("submit", async (event) => {
     event.preventDefault();
     const searchText = searchTextEl.value;
-    moviesData = getMoviesData(searchText);
+    moviesData = await setMoviesData(searchText);
+    renderMovies();
 });
 
-async function getMoviesData(searchText) {
-    const res = await fetch(`http://www.omdbapi.com/?s=${searchText}&apikey=83ab7c1e`);
-    const data = await res.json(); // Search: Array(10), totalResults: '1015', Response: 'True'
-    return data.Response ? data.Search : [];
+async function setMoviesData(searchText) {
+    const resultData = [];
+    const res = await fetch(
+        `http://www.omdbapi.com/?s=${searchText}&apikey=83ab7c1e&type=movie`
+    );
+    const data = await res.json();
+    if (data.Response === "True") {
+        for (let movie of data.Search.slice(0, 2)) {
+            const res = await fetch(
+                `http://www.omdbapi.com/?i=${movie.imdbID}&apikey=83ab7c1e&type=movie&plot=short`
+            );
+            const data = await res.json();
+            resultData.push(data);
+        }
+    }
+    return resultData;
+}
+
+function renderMovies() {
+    let renderHtml = "";
+    if (moviesData.length > 0) {
+        renderHtml = moviesData.map(movie => new Movie(movie).getMovieHtml()).join("");
+        moviesContainer.classList.add("full-height");
+    } else {
+        renderHtml = `
+        <div class="not-found">
+        Unable to find what you're looking 
+        for.
+        Please try another search.
+        </div>
+        `
+        moviesContainer.classList.remove("full-height");
+    }
+    moviesContainer.innerHTML = renderHtml;
 }
